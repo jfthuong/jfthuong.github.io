0e527d7d60279a97819e8e82cf8e68db3f50ec64
Jean-Francois Thuong <jfthuong@gmail.com>
2018-08-24 13:45:05 +0800

    Improved docstrings

--- Python_Practice/Solution/level_FINAL/airlines.py
+++ Python_Practice/Solution/level_FINAL/airlines.py
@@ -1,27 +1,29 @@
 #!python
 
-# #-- CALCULATION OF TIME DELTA --##
+from collections import defaultdict
 from datetime import datetime
 
 
-def get_delay(time, takeOff):
-    """Measure the time difference between two times
-    >>> get_delay('12:10','12:45')
-    35
-    >>> get_delay('12:10','11:45')
-    -25
+# #-- CALCULATION OF TIME DELTA --##
+def get_delay(expected:str, takeoff:str) -> int:
+    """\
+    Measure the time difference between two times
+    delay = <take-off> - <expected>
+
+    Args:
+        expected: expected departure time (e.g. 23:45)
+        takeoff: real departure time (e.g. 23:55)
     """
-    format = "%H:%M"
-    delta = datetime.strptime(takeOff, format) - datetime.strptime(time, format)
+    fmt = "%H:%M"
+    delta = datetime.strptime(takeoff, fmt) - datetime.strptime(expected, fmt)
     # Calculate delay (can be negative - first case)
     if delta.days < 0:
-        delay = (delta.seconds - 60 * 60 * 24) / 60
+        sec_in_day = 60 * 60 * 24
+        delay = (delta.seconds - sec_in_day) / 60
     else:
         delay = delta.seconds / 60
     return int(delay)
 
-
-# #-------------------------------##
 
 
 
@@ -32,13 +34,19 @@ class Airline():
         """Creation of an airline with records"""
         self.name = name
         self.code = code
-        self.flights = dict()
+        self.flights = defaultdict(list)
         self.destination = dict()
 
     def add_info(self, record):
-        """Add record to the list of flights
-            (dict) -> None
-            record = dictionary with keys: 'date', 'time', 'code', 'airline', 'destination', 'take-off'
+        """Add flight information, create flight if it does not exist
+
+        Args:
+            record(dict): dictionary with keys:
+                * 'date': date of the flight, stored as a string (e.g. "2018-02-30")
+                * 'time': expected take-off time, stored as a string (e.g. "15:30")
+                * 'code': code of flight (e.g. "MU123")
+                * 'destination'
+                * 'take-off': real take-off time, stored as a string
         """
         code = record["code"]
 
@@ -46,49 +54,42 @@ class Airline():
         if code not in self.destination:
             self.destination[code] = record["destination"]
 
-            # Record to save in the list of records
+        # Record to save in the list of records
         saved_record = {k: record[k] for k in ("date", "time")}
+        # NOTE: equivalent to: saved_record = {"date":record["date"], "time":record["time"]}
         saved_record["delay"] = get_delay(record["time"], record["take-off"])
 
         # Save record
-        if code in self.flights:
-            self.flights[code].append(saved_record)
-        else:
-            self.flights[code] = [saved_record]
+        self.flights[code].append(saved_record)
 
-    def get_rating_flight(self, flight):
+    def get_rating_flight(self, flight:str):
         """Get the rating of a given flight
-           (string) -> ('% late':int, 'avg delay':int)
+
+        Args:
+            flight: code of a flight
+
+        Returns:
+            ('% late', 'average delay') for a flight, both int
         """
         # Flight unknown
         if flight not in self.flights:
             return None, None
-            # Known flight: calculate number of flights late and total delay
+        # Known flight: calculate number of flights late and total delay
         nb_late, total_delay = 0.0, 0.0
         for record in self.flights[flight]:
             total_delay += record["delay"]
             if record["delay"] > 30:
                 nb_late += 1
-                # Return result
+
         percent = int(nb_late / len(self.flights[flight]) * 100)
         average = int(total_delay / len(self.flights[flight]))
         return percent, average
 
     def get_rating_airline(self):
         """Get the rating of the airline
-        (None) -> ('% late':int, 'avg delay':int)
 
-        >>> ChinaEastern=Airline('China Eastern',  'MU')
-        >>> ChinaEastern.add_info({'date':'2015-08-24', 'time':'12:00', 'code':'MU155', 'destination':'CDG', 'take-off':'13:46'})
-        >>> ChinaEastern.get_rating_airline()
-        (100, 106)
-        >>> AirFrance=Airline('Air France',  'AF')
-        >>> AirFrance.add_info({'date':'2015-08-24', 'time':'12:00', 'code':'AF117', 'destination':'CDG', 'take-off':'13:46'})
-        >>> AirFrance.add_info({'date':'2015-08-25', 'time':'12:00', 'code':'AF117', 'destination':'CDG', 'take-off':'12:16'})
-        >>> AirFrance.add_info({'date':'2015-08-24', 'time':'12:00', 'code':'AF120', 'destination':'JFK', 'take-off':'11:46'})
-        >>> AirFrance.add_info({'date':'2015-08-25', 'time':'12:00', 'code':'AF120', 'destination':'JFK', 'take-off':'15:46'})
-        >>> AirFrance.get_rating_airline()
-        (50, 83)
+        Returns:
+            ('% late', 'average delay') for the airline, both int
         """
         nb_late, total_delay, total_flights = 0.0, 0.0, 0
         # Get rating of airline based on ratings from flights
